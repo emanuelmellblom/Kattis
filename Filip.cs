e418a191e301186a2190643b816cb30ab05c595b
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2018-11-25 21:49:39 +0100

    New solutions

---
+++ Filip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// Problem Filip read, reverse and compare numbers
+
+namespace Kattis_Programming_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var separatedInput = input.Split(' ');
+            Console.WriteLine(Int32.Parse(Reverse(separatedInput[0])) > Int32.Parse(Reverse(separatedInput[1])) ? Int32.Parse(Reverse(separatedInput[0])) : Int32.Parse(Reverse(separatedInput[1])));
+        }
+
+        public static string Reverse(string toConvert){
+            char[] asArray = toConvert.ToCharArray();
+            Array.Reverse(asArray);
+            return new string(asArray);
+        }
+    }
+}
---
+++ Ive_Been_Everywhere_Man.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ive_Been_Everywhere_Man
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = Int32.Parse(Console.ReadLine());
+            List<int> outPut = new List<int>();
+            HashSet<string> tmpCollection = new HashSet<string>();
+
+            for (int i = 0; i < numberOfInputs; i++){
+                int len = Int32.Parse(Console.ReadLine());
+                for (int x = 0; x < len; x++){
+                    tmpCollection.Add(Console.ReadLine());
+                }
+                outPut.Add(tmpCollection.Count);
+                tmpCollection.Clear();
+            }
+            foreach(int value in outPut){
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
