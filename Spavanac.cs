cb46d794dc90cf926320e01891d925ccbf18424b
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2018-12-16 20:08:44 +0100

    new solution

---
+++ Spavanac.cs
@@ -0,0 +1,21 @@
+﻿//https://open.kattis.com/problems/spavanac
+using System;
+
+namespace Spavanac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputTime = Console.ReadLine().Split(' ');
+            int hour = Int16.Parse(inputTime[0]);
+            int minutes = Int16.Parse(inputTime[1]) - 45;
+            if(minutes < 0){
+                minutes += 60;
+                hour--;
+            }
+            if (hour < 0) hour = 23;
+            Console.WriteLine($"{hour} {minutes}");
+        }
+    }
+}
