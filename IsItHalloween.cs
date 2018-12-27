6232a559c5ad0077ed0f3049bc21343d364f7a83
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2018-12-27 22:17:55 +0100

    new

---
+++ IsItHalloween.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IsItHalloween
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if(input == "OCT 31" || input == "DEC 25"){
+                Console.WriteLine("yup");
+            }else{
+                Console.WriteLine("nope");
+            }
+        }
+    }
+}
