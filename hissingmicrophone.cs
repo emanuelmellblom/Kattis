b4c6dd85ea0c8d570489b0c070b28beb0170eca2
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2019-01-20 20:49:38 +0100

    new

---
+++ hissingmicrophone.cs
@@ -0,0 +1,19 @@
+﻿// https://open.kattis.com/problems/hissingmicrophone
+
+using System;
+
+namespace HissingMicrophone
+{
+    class hissingmicrophone
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if(input.Contains("ss")){
+                Console.WriteLine("hiss");
+            }else{
+                Console.WriteLine("no hiss");
+            }
+		}
+    }
+}
